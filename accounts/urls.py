020a42ea39f4802d09561d65b98e0b9db6391904
rabBit64 <gojh47@gmail.com>
2023-04-27 14:17:24 +0900

    feat: accounts base

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    
+    #path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('delete/', views.delete, name='delete'),
+    path('profile/<int:pk>', views.profile, name='profile'),
+    path('edit/<str:user>', views.edit, name='edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .forms import CustomAuthenticationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            print(request.user)
+            auth_login(request, form.get_user()) 
+            return redirect('articles:index')
+    else:
+        form = CustomAuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+@login_required
+def profile(request,pk):
+    User_detail = get_user_model().objects.get(pk=pk)
+    context = {
+        'User_detail':User_detail,
+    }
+    return render(request,'accounts/profile.html',context)
+
+@login_required
+def edit(request, user):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else: 
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form' : form,
+        'user': user,
+    }
+    return render(request,'accounts/update.html', context)
+
+@login_required
+def delete(request):
+    user = request.user
+    user.delete()
+    logout(request)
+    return redirect(request,'articles:index')
