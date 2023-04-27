18ef73bb19809b9e574a13aac2f519479f93ef17
Kurman11 <skedyd45@naver.com>
2023-04-27 15:03:10 +0900

    articles/base

--- articles/models.py
+++ articles/models.py
@@ -9,4 +9,11 @@ class Article(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    review = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
-
+from .forms import ArticleForm , CommentForm
 # Create your views here.
 
 def init(request):
@@ -14,4 +14,67 @@ def index(request):
         'articles' : articles,
     }
     return render(request, 'articles/index.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'articles/create.html',context)
+
+
+def detail(request,article_pk):
+    article = Article.objects.get(pk = article_pk)
+    context = {
+        'article': article
+    }
+    return render(request,'articles/detail.html',context)
+
+def update(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail',article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form' : form
+    }
+    return render(request,'articles/update.html',context)
+
+
+def delete(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.user == article.user:
+        article.delete()
+    return redirect('articles:index')
+
+
+def create_comment(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.user = request.user
+        comment.review = article
+        comment.save()
+        return redirect('articles:detail', article.pk)
+    context = {
+        'article' : article,
+        'comment_form' : comment_form,
+    }
+    return render(request,'articles/detail.html',context)
+
+
 
