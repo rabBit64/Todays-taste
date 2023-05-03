0d7e03358584932793005fc58a2b295a57f24b5b
rabBit64 <gojh47@gmail.com>
2023-05-03 12:41:42 +0900

    Fix product model

--- articles/forms.py
+++ articles/forms.py
@@ -27,6 +27,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',) 
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows':1, })
+        }
 
 
 
