9adfd0aa3d14a660a71e3e4d69f1e5ce8923bb8c
rabBit64 <gojh47@gmail.com>
2023-05-05 15:38:42 +0900

    fix: product 모델 수정(별점,상품 지정)

--- articles/views.py
+++ articles/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from accounts.models import User
 from django.core.paginator import Paginator
-
+from django.http import JsonResponse
 # Create your views here.
 
 
