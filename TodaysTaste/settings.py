ac90af1bee699f26da85a7372e622519917e34ba
ldwlgus12 <ldwlgus12@naver.com>
2023-05-08 08:14:26 +0900

    service

--- TodaysTaste/settings.py
+++ TodaysTaste/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n34gw5tlpy3^w3)3v6!vi=0kf6ne(&kq%k58cp6t!d6tyc*k$6'
+# SECRET_KEY = 'django-insecure-n34gw5tlpy3^w3)3v6!vi=0kf6ne(&kq%k58cp6t!d6tyc*k$6'
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-todays-taste-4c7jj2blhe0wpv6.sel4.cloudtype.app', 'localhost']
 
 
 # Application definition
